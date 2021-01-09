d00d04950fb472941f75b638424722bba2444e47
El'endia Starman <lee314b@gmail.com>
2021-01-09 15:09:02 -0500

    Some more work on the parser.

--- examples/run_example.py
+++ examples/run_example.py
@@ -1,52 +1,78 @@
 import sys
 import re
 
+from ..system import PyedSTDIN
+
+
+class BuiltInParser:
+  def consume(self, _input, stack):
+    name = ''
+
+    for char in _input:
+      if char == ' ':
+        break
+      else:
+        name += char
+
+    if name == 'STDIN':
+      node = PyedSTDIN()
+    elif name == 'LOOP':
+      # node = PyedSTDIN()
+      ...
+    else:
+      raise ValueError(f'Unknown builtin: `{name}`')
+
+    return node, None
+
+
+class LabelParser:
+  def consume(self, _input, stack):
+    label = ''
+
+    for char in _input:
+      if char == ':':
+        return label, ExpressionParser()
+      elif char == '/':
+        return None, None
+      else:
+        label += char
+
+
+class ExpressionParser:
+  def consume(self, _input, stack):
+    for char in _input:
+      if char == '$':
+        return None, [self, BuiltInParser()]
+      elif char == '\n':
+        return None, [self, LabelParser()]
+
+
+class ProgramParser:
+  def __init__(self, parsers=None):
+    self.parsers = [] or parsers
+    self.stack = []
+    self.nodes = []
+
+  def parse(self, _input):
+    curr_parser = self.stack.pop()
+    node, parsers = curr_parser.consume(_input, self.stack)
+
+    if node:
+      self.nodes.append(node)
+
+    if parsers:
+      self.stack.extend(parsers)
+
 
 if __name__ == '__main__':
   args = sys.argv
   if len(args) < 2 or args[1].startswith('-'):
     print('Run it like this:')
-    print('python run_example.py 00_truth_machine.pyed')
+    print('python run_example.py 00_truth_machine.pyedasm')
 
   filepath = args[1]
   with open(filepath, 'r') as file:
     contents = [*file]
 
-  errors = []
-
-  for index, line in enumerate(contents):
-    print(f'[{index}]', line)
-    # we're gonna parse each line in a super lazy way
-    # apply regexes until one matches, then:
-    #  * make a node
-    #  * chop off the prefix and continue
-    line_errors = []
-
-    _str_id = re.match(r'(\w*):', line)
-    if _str_id is None:
-      line_errors.append('no colon!')
-      continue
-    else:
-      _str_id = _str_id.groups()[0]
-      print(f'_str_id: `{_str_id}`')
-      line = line[len(_str_id) + 1:]
-
-    args = []
-    kwargs = {}
-    while line:
-      line = line.strip()
-      print('line:', line)
-
-      arg = re.match(r'(\$[A-Z]+|(\w+=)?(:?\w+))', line)
-      print('arg:', arg)
-
-      if arg is None:
-        break
-      else:
-        print(arg.groups())
-        line = line[len(arg.groups()[0]) + 1:]
-
-    errors.append(line_errors)
-
-  for err in errors:
-    print('Error:', err)
+  print('contents:')
+  print(contents)
